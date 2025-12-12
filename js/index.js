90f32e5bce1bde64f4a6dc46b074eae2323aeb52
MrCadillac <m.gordon.smith@hotmail.com>
2025-12-11 23:20:20 -0500

    changed some js
    changed some js to see if this is the reason why it wont scroll on iphone chrome browser

--- js/index.js
+++ js/index.js
@@ -2,16 +2,36 @@
 
 
 
-let lastScrollTop = 0; //initial position of the header
-const header = document.querySelector('.header'); 
+//let lastScrollTop = 0; //initial position of the header
+//const header = document.querySelector('.header'); 
 
-window.addEventListener('scroll', function(){
-    let scrollTop = this.window.pageYOffset || document.documentElement.scrollTop;
-    if(scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+//window.addEventListener('scroll', function(){
+ //   let scrollTop = this.window.pageYOffset || document.documentElement.scrollTop;
+ //   if(scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+  //      header.classList.add('hidden');
+  ///  } else {
+   //     header.classList.remove('hidden');
+  //  }
+   // lastScrollTop = scrollTop;
+   // console.log('it happened')
+//});
+let lastScrollTop = 0;
+const header = document.querySelector('.header');  // This is fine since you have class="header"
+
+function handleScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+
+    // scrolling down
+    if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
         header.classList.add('hidden');
-    } else {
+    } 
+    // scrolling up
+    else {
         header.classList.remove('hidden');
     }
-    lastScrollTop = scrollTop;
-    console.log('it happened')
-});
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // avoid negative values
+}
+
+window.addEventListener('scroll', handleScroll, { passive: true });
+window.addEventListener('touchmove', handleScroll, { passive: true });
